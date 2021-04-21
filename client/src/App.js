581c28cbe3502bccc41cac0c2cc22012d1ac590c
Damarowen <67860422+Damarowen@users.noreply.github.com>
2021-04-21 20:32:13 +0700

    section 2 done : add product page based id

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,26 @@
-import React, { Fragment} from 'react'
+import React from 'react'
+import { BrowserRouter as Router , Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import ProductScreens from './screens/ProductScreens'
+import Homescreen from './screens/Homescreen'
 
 const App = () => {
 
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
           <h1>hi damar</h1>
+          <Route path='/' component={Homescreen} exact/>
+          <Route path='/product/:id' component={ProductScreens} exact/>
+
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   )
 }
 
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,22 +1,29 @@
 import React from 'react'
-import { Navbar,Nav, Container } from 'react-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 const Header = () => {
     return (
-        <div>
+        <>
             <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
                 <Container>
-                <Navbar.Brand href="/">Proshop</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto">
-                        <Nav.Link href="/login"><i class="fas fa-cart-plus" aria-hidden="true"></i> Cart</Nav.Link>
-                        <Nav.Link href="/cart"> <i class="fas fa-user" aria-hidden="true"></i>   Sign In</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand >Proshop</Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                            <LinkContainer to="/cart">
+                                <Nav.Link ><i className="fas fa-cart-plus" aria-hidden="true"></i> Cart</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/login">
+                                <Nav.Link > <i className="fas fa-user" aria-hidden="true"></i>   Sign In</Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
-        </div>
+        </>
     )
 }
 
